f1610c70b86d0b70b634dd510b19f566e8b949f8
Alesya Huzik <alesya@alesya.me>
2026-03-30 21:16:16 +1100

    1.1.0

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const SERVER_NAME = "jsmcp";
-export const SERVER_VERSION = "1.0.0";
+export const SERVER_VERSION = "1.1.0";
 export const DEFAULT_PRESET = "default";
 export const DEFAULT_DISCOVERY_TIMEOUT_MS = 5000;
 export const DEFAULT_CODE_TIMEOUT_MS = 30000;
